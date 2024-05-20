2e8a99ed41632296992978d3ec808289ff397492
tanvir70 <rifattanvir70@gmail.com>
2024-05-20 19:39:57 +0600

    Replacing Statement with PrepareStatement

--- src/main/java/ProductRepositoryImpl.java
+++ src/main/java/ProductRepositoryImpl.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -8,16 +9,25 @@ public class ProductRepositoryImpl implements ProductRepository{
     private DBConnection dbConnection = new DBConnection();
     @Override
     public void save(Product product) {
-        var sql = String.format("INSERT INTO product (name, description, price, date_created, date_last_updated) VALUES ('%S', '%S', %s, '%s', '%s')",
-        product.getName(),
-                product.getDescription(),
-                product.getPrice(),
-                product.getVersion(),
-                Timestamp.valueOf(product.getDateCreated()),
-                Timestamp.valueOf(product.getDateLastUpdated()));
+        var sql = "INSERT INTO product ( " +
+                "name, " +
+                "description, " +
+                "price, " +
+                "version, " +
+                "date_created, " +
+                "date_last_updated) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
 
-        try (var connection = dbConnection.tryConnection(); var statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
+        try (var connection = dbConnection.tryConnection(); PreparedStatement pstmnt = connection.prepareStatement(sql)) {
+            pstmnt.setString(1, product.getName());
+            pstmnt.setString(2, product.getDescription());
+            pstmnt.setBigDecimal(3, product.getPrice());
+            pstmnt.setLong(4, product.getVersion());
+            pstmnt.setTimestamp(5, Timestamp.valueOf(product.getDateCreated()));
+            pstmnt.setTimestamp(6, Timestamp.valueOf(product.getDateLastUpdated()));
+
+            pstmnt.execute();
+            System.out.println("Product info inserted successfully");
         } catch (Exception e) {
             throw new RuntimeException("Unable to insert product info to database", e);
         }
