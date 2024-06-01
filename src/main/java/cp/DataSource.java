6de98a378d8bfccd4ded69b3290a4b95477a1300
tanvir70 <rifattanvir70@gmail.com>
2024-06-01 14:59:34 -0400

    Data Source

---
+++ src/main/java/cp/DataSource.java
@@ -0,0 +1,12 @@
+package cp;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+    private static JdbcConnectionPool pool
+            = new JdbcConnectionPool();
+    public static Connection getConnection() throws SQLException {
+        return pool.getConnectionFromPool();
+    }
+}
